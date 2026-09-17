a0518703a11594be8112b6fb48acffad0d020a3a
cronos-2026 <yufiw0704@gmail.com>
2026-09-17 20:07:00 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /* 正式使用前請以 config.example.js 內容覆蓋本檔並填入設定。 */
 window.MINGPIAN_CONFIG = {
-  googleClientId: "",
+  googleClientId: "526437660432-v5ajr1nrm7do2134pg5r28ku7u55if5u.apps.googleusercontent.com",
   ocrWorkerUrl: "",
   ocrSpaceApiKey: "K85274205088957"
 };
